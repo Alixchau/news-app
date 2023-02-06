aa452216df5b244f52b899e2d8f4dd961876bbc6
alix chau <alixchau31@gmail.com>
2023-02-05 22:47:21 -0700

    initial setup and add hearder component

---
+++ app/NavLinks.tsx
@@ -0,0 +1,23 @@
+//in order to use hooks(usePathname), need to make the component a client component
+'use client' 
+
+import { categories } from "../constants"
+import NavLink from "./NavLink"
+import {usePathname} from "next/navigation"
+
+function NavLinks() {
+    const pathname = usePathname();
+
+    const isActive = (path: string) => {
+        return pathname?.split('/')[2] === path;
+    }
+  return (
+      <nav className="grid grid-cols-4 md:grid-cols-7 text-xs md:text-sm max-w-6xl gap-4 border-b pb-8 mx-auto">
+          {categories.map(category => (
+              <NavLink key={category} category={category} isActive={isActive(category)} />
+          ))}
+   </nav>
+  )
+}
+
+export default NavLinks
