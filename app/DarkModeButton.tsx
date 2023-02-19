e4d2c9742092ccf883fde8dc635f52be9e678aa0
Alix <alixchau831@gmail.com>
2023-02-19 14:58:49 -0700

    revise darkModeBtn component

--- app/DarkModeButton.tsx
+++ app/DarkModeButton.tsx
@@ -20,18 +20,13 @@ function DarkModeButton() {
         <div>
           
             {currentTheme === "dark" ? (
-             <div className='flex align-middle items-center justify-center  cursor-pointer space-x-2'  onClick={() => setTheme("light")} >    
-                <p>Light Mode</p>
+
                 <SunIcon className='h-5 w-5 text-yellow-500'
                    />
-                    </div>
             ) : (
-                    <div className='flex align-middle items-center justify-center cursor-pointer space-x-2'  onClick={() => setTheme("dark")
-                } >
-                        <p>Dark Mode</p>
+
                     <MoonIcon className='h-5 w-5 text-gray-900'
                        />
-                    </div>
             )}
         </div>
     );
