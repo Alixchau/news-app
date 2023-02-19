0640559c4044867f441481c9bc7ae70f89ded650
Alix <alixchau831@gmail.com>
2023-02-19 13:04:10 -0700

    Implement dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css"
 import Header from "./Header"
+import Providers from "./Providers"
 
 export default function RootLayout({
   children,
@@ -9,11 +10,14 @@ export default function RootLayout({
   return (
     <html>
         <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-75">
-          {/* Header will remind as server component */}
+        {/* Header will remind as server component */}
+        <Providers>
+
         <Header />
         <div className="max-w-6xl mx-auto">
           {children}
         </div> 
+        </Providers>
       </body>
     </html>  
   )
